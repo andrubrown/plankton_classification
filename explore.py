eebfa872264dbcd426b963c2323afd58c7d4dd36
Michael Mortonson <michael.mortonson@gmail.com>
2015-01-26 13:41:47 -0800

    Separate code for random training images into a function

--- explore.py
+++ explore.py
@@ -7,21 +7,25 @@ Created on Sun Jan 25 18:59:11 2015
 
 import numpy as np
 import matplotlib.pyplot as plt
-from image_files import TrainImages
 
-n_rows = 3
-n_cols = 3
 
-plt.rcParams['figure.figsize'] = (7, 7)
+def random_train_images(seed, size):
+    from image_files import TrainImages
 
-np.random.seed(1251902)
+    n_rows = size[0]
+    n_cols = size[1]
 
-train = TrainImages()
+    plt.rcParams['figure.figsize'] = (7, 7)
 
-for i, image in enumerate(train.get_random_images(n_rows*n_cols)):
-    plt.subplot(n_rows, n_cols, i)
-    image.plot()
-    plt.title(train.get_class_by_id(image.get_id()),
-              fontdict={'fontsize': 8})
+    np.random.seed(seed)
 
-plt.tight_layout(1.08, h_pad=1, w_pad=0)
+    train = TrainImages()
+
+    for i, image in enumerate(train.get_random_images(n_rows*n_cols)):
+        plt.subplot(n_rows, n_cols, i+1)
+        image.plot()
+        plt.title(train.get_class_by_id(image.get_id()),
+                  fontdict={'fontsize': 9})
+
+    plt.tight_layout(1.08, h_pad=1, w_pad=0)
+    plt.show()
