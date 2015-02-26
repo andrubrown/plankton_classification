ea0e5b351ae37033bc761a4432b7af462c36db4a
Michael Mortonson <michael.mortonson@gmail.com>
2015-02-26 14:03:47 -0800

    Handle region properties that return lists of values

--- feature_table.py
+++ feature_table.py
@@ -10,7 +10,7 @@ from image_files import TrainImages, TestImages
 
 
 class FeatureTable(object):
-    def __init__(self, sample):
+    def __init__(self, sample, max_rows=None):
         if sample == 'train':
             self.image_set = TrainImages()
         elif sample == 'test':
@@ -18,19 +18,24 @@ class FeatureTable(object):
         else:
             raise ValueError
         ids = self.image_set.get_all_ids()
+        if max_rows is not None:
+            ids = ids[:max_rows]
         classes = [self.image_set.get_class_by_id(int(i)) for i in ids]
         self.data = pd.DataFrame(data={'class': classes}, index=ids)
 
     def add_largest_region_properties(self, properties):
         table = {}
-        for p in properties:
-            table[p] = []
         for i in self.data.index:
             image = self.image_set.get_image_by_id(int(i))
             region_props = image.get_largest_region_properties()
             for p in properties:
                 value = getattr(region_props, p)
-                table[p].append(value)
+                try:
+                    for j, v in enumerate(value):
+                        key = p + str(j)
+                        table[key] = table.get(key, []) + [v]
+                except:
+                    table[p] = table.get(p, []) + [value]
         self.data = pd.concat([self.data,
                                pd.DataFrame(data=table,
                                             index=self.data.index)],
