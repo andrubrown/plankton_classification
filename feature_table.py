021e6d0810df0456c41d3e50c16d4b695a0a7e13
Michael Mortonson <michael.mortonson@gmail.com>
2015-02-26 16:12:45 -0800

    Add pair plotting method to FeatureTable

--- feature_table.py
+++ feature_table.py
@@ -5,7 +5,10 @@ Created on Thu Feb 19 17:20:11 2015
 @author: mmortonson
 """
 
+import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
 from image_files import TrainImages, TestImages
 
 
@@ -22,8 +25,9 @@ class FeatureTable(object):
             ids = ids[:max_rows]
         classes = [self.image_set.get_class_by_id(int(i)) for i in ids]
         self.data = pd.DataFrame(data={'class': classes}, index=ids)
+        self.scaler = None
 
-    def add_largest_region_properties(self, properties):
+    def add_largest_region_properties(self, properties, scaler=None):
         table = {}
         for i in self.data.index:
             image = self.image_set.get_image_by_id(int(i))
@@ -36,7 +40,27 @@ class FeatureTable(object):
                         table[key] = table.get(key, []) + [v]
                 except:
                     table[p] = table.get(p, []) + [value]
-        self.data = pd.concat([self.data,
-                               pd.DataFrame(data=table,
-                                            index=self.data.index)],
-                              axis=1)
+        new_data = pd.DataFrame(data=table, index=self.data.index)
+        indices = new_data.index
+        columns = new_data.columns
+        if scaler is None:
+            self.scaler = StandardScaler()
+            new_data = self.scaler.fit_transform(new_data)
+        else:
+            self.scaler = scaler
+            new_data = self.scaler.transform(new_data)
+        new_data = pd.DataFrame(new_data, index=indices, columns=columns)
+        self.data = pd.concat([self.data, new_data], axis=1)
+
+    def pair_plot_random_classes(self, n=2, properties=None, seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+        all_classes = self.data['class'].unique()
+        classes = np.random.choice(all_classes, n, replace=False)
+        rows = self.data['class'].isin(classes)
+        if properties is None:
+            plot_data = self.data[rows]
+        else:
+            plot_data = self.data[rows][['class'] + properties]
+        sns.pairplot(plot_data, hue='class', size=9.0/len(plot_data.columns),
+                     diag_kind='kde', palette='Set2', alpha=0.7)
