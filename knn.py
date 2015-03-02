ff6e71446971dfdb0d3630a1de091f273a4b704f
Michael Mortonson <michael.mortonson@gmail.com>
2015-03-01 20:00:04 -0800

    Save and load computed features in csv files

--- knn.py
+++ knn.py
@@ -5,57 +5,95 @@ Created on Sat Feb 28 17:37:02 2015
 @author: mmortonson
 """
 
+import sys
+from os.path import isfile
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.grid_search import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from feature_table import FeatureTable
-from sklearn.grid_search import GridSearchCV
 
 features = ['solidity', 'max_intensity', 'min_intensity',
             'filled_area', 'euler_number', 'eccentricity']
 
+prediction_file = 'predictions/test_prob_knn_weighted.csv'
 
-# load training data and compute features
-print 'Loading training data'
-train = FeatureTable('train')
-print 'Computing features'
-train.add_largest_region_properties()
+train_file = 'train_features.csv'
+scaler_file = 'feature_scaling.csv'
+test_file = 'test_features.csv'
 
-# save features and scaling parameters
-train.data.to_csv('train_features.csv')
-print 'Feature scaling:'
-print 'mean values:'
-print train.scaler.mean_
-print 'standard deviations:'
-print train.scaler.std_
 
-# split data into class labels and features
-train_classes = train.data['class']
-train_features = train.data.drop(['class'], axis=1)
+# load training data and compute features if necessary
+if isfile(train_file):
+    print 'Loading training data features from {0}'.format(train_file)
+    train_data = pd.read_csv(train_file, index_col=0)
+    train_classes = train_data['class']
+    train_features = train_data[features]
+    if not isfile(test_file):
+        if isfile(scaler_file):
+            scaler_parameters = pd.read_csv(scaler_file)
+            scaler_mean = np.array(scaler_parameters['mean'])
+            scaler_std = np.array(scaler_parameters['std'])
+            scaler = StandardScaler()
+            scaler.mean_ = scaler_mean
+            scaler.std_ = scaler_std
+        else:
+            sys.exit('Missing scaling parameters from training data.')
+else:
+    print 'Loading training data'
+    train = FeatureTable('train')
+    print 'Computing features'
+    train.add_largest_region_properties()
+    scaler = train.scaler
+
+    # save features and scaling parameters
+    train.data.to_csv(train_file)
+    if not isfile(scaler_file):
+        scaler_df = pd.DataFrame({'mean': scaler.mean_, 'std': scaler.std_})
+        scaler_df.to_csv(scaler_file)
+
+    # split into class labels and features
+    train_classes = train.data['class']
+    train_features = train.data[features]
+
 
 # optimize k-NN classifier by finding k with best log-loss on CV samples
-print 'Optimizing k-NN'
-knn = KNeighborsClassifier()
-knn_opt = GridSearchCV(knn, {'n_neighbors': [800, 850, 900, 950]},
-                       scoring='log_loss')
-knn_opt.fit(train_features, train_classes)
-print knn_opt.grid_scores_
+knn = KNeighborsClassifier(weights='distance')
+n_neighbors = []
+while len(n_neighbors) != 1:
+    input_string = raw_input('\nEnter multiple values of k to try with CV,' +
+                             '\nor one value to train the k-NN classifier:\n')
+    n_neighbors = [int(x) for x in input_string.split()]
+    knn_opt = GridSearchCV(knn, {'n_neighbors': n_neighbors},
+                           scoring='log_loss', cv=5, verbose=1)
+    knn_opt.fit(train_features, train_classes)
+    if len(n_neighbors) > 1:
+        print 'log loss estimates:'
+        for cv_score in knn_opt.grid_scores_:
+            print cv_score
 
 # load test data and compute features, using scaling from training data
-print 'Loading test data'
-test = FeatureTable('test')
-print 'Computing features'
-test.add_largest_region_properties(features, train.scaler)
-test_features = test.data.drop(['class'], axis=1)
+if isfile(test_file):
+    print '\nLoading test data features from {0}'.format(test_file)
+    test_data = pd.read_csv(test_file, index_col=0)
+    test_features = test_data[features]
+    test_index = test_data.index
+else:
+    print '\nLoading test data'
+    test = FeatureTable('test')
+    print 'Computing features'
+    test.add_largest_region_properties(scaler=scaler)
+    test_features = test.data[features]
+    test_index = test.data.index
 
-# save features
-test.data.to_csv('test_features.csv')
+    # save features
+    test.data.to_csv(test_file)
 
-# predict class probabilities for test data
+print 'Predicting class probabilities for test data'
 p_class = knn_opt.predict_proba(test_features)
-
-# write probabilities to output file
 classes = sorted(np.unique(train_classes))
-files = [str(i) + '.jpg' for i in test.data.index]
+files = [str(i) + '.jpg' for i in test_index]
 p_class_df = pd.DataFrame(p_class, index=files, columns=classes)
-p_class_df.to_csv('predictions/test_prob_knn.csv', index_label='image')
+p_class_df.to_csv(prediction_file, index_label='image')
+print 'Probabilities written to {0}'.format(prediction_file)
